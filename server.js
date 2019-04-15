e11c487936c33e1f1c45e120382ec01c92d4e21f
jamie broberg <angelfire529@yahoo.com>
2019-04-14 22:40:08 -0700

    testing

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ app.enable("trust proxy");
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Header', 'X-FORWARDED-PROTO')
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, X-Forwarded-Proto");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     next();
 })
 
