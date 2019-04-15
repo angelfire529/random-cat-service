f404854f4ad70e9c790fa228659f5f8c97eac60c
jamie broberg <angelfire529@yahoo.com>
2019-04-14 22:32:55 -0700

    testing

--- server.js
+++ server.js
@@ -4,13 +4,10 @@ var app = express();
 
 app.enable("trust proxy");
 
-function removeHTTPS(req, res, next) {
-    if (!req.secure && req.get('x-forwarded-proto') !== 'http') {
-        return res.redirect('http://' + req.get('host') + req.url);
-    }
-}
-
-// app.use(removeHTTPS)
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+})
 
 app.get('/', function (req, res) {
      res.json({'greeting': 'hello'})
@@ -21,6 +18,7 @@ app.get('/api/cat', function(req, res) {
     request.get('http://aws.random.cat/meow', function (error, resp, body) {
         if (!error) {
             const response = JSON.parse(body).file;
+
            res.json(response);
         }
         else 
