54f5992dce3700832478537a6ff0616c4bd48814
jamie broberg <angelfire529@yahoo.com>
2019-04-14 21:32:54 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express')
+var request = require('request');
+var app = express();
+
+app.enable("trust proxy");
+
+function removeHTTPS(req, res, next) {
+    if (!req.secure && req.get('x-forwarded-proto') !== 'http') {
+        return res.redirect('http://' + req.get('host') + req.url);
+    }
+}
+
+app.use(removeHTTPS)
+
+
+app.get('*', function(req, res) {
+    // res.send('hello')
+    request.get('http://aws.random.cat/meow', function (error, resp, body) {
+        if (!error) {
+            const response = JSON.parse(body).file;
+           res.json(response);
+        }
+        else 
+        res.json(error);
+    })
+});
+
+app.listen(5000, function(req, res) {
+    console.log('server is running on port: ', 5000);
+})
